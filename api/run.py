c14129947dd5004e3d5d3caf19a6ef3915a2eef7
Mykola Basov <kolybasov@gmail.com>
2017-02-06 23:20:13 +0200

    Add SQLite DB connection and template

--- api/run.py
+++ api/run.py
@@ -1,10 +1,15 @@
 import yaml
 import io
+import sqlite3
 
 from flask import Flask
 
 # Parse config
 config = yaml.load(io.open('./config.yml', 'r'))
+
+# Connect to DB
+connection = sqlite3.connect('./ankor_storage.db')
+db = connection.cursor()
 
 # Create Flask app
 app = Flask(__name__)
